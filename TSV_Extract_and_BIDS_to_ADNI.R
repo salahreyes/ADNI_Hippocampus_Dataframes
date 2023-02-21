656055eaf4fb59edc75f38e5f55fe65205057dbb
salahreyes <salahreyes@gmail.com>
2023-02-20 20:39:57 -0500

    Implemented a script that replaces the subject column with ADNI identifier.

---
+++ TSV_Extract_and_BIDS_to_ADNI.R
@@ -0,0 +1,36 @@
+```{r}
+file_list <- list.files(path = "/Volumes/LeCie/HippunFold_Outputs/Screening", 
+                        pattern = "*volumes*.tsv", 
+                        full.names = TRUE, 
+                        recursive = TRUE)
+file_list
+```
+
+
+```{r}
+# Empty data frame
+volumes_dt <- data.frame(NULL)
+
+for(file in file_list){
+  # Read the file
+  temp_dt <- data.table::fread(file, na.strings = c("NA", "NaN", "", "?"))
+  
+  # Change the subject's ID from BIDS' to ADNI's
+  
+  # Split file path by /
+  a <- strsplit(file, 
+                split = "/")[[1]][6]
+  # Split file path by -
+  b <- strsplit(a, 
+                split = "-")
+  # Assign ID
+  temp_dt$subject <- substring(b[[1]][2], 
+                               first = 1, 
+                               last=10)
+  
+  volumes_dt <- dplyr::bind_rows(volumes_dt, temp_dt)
+}
+
+
+```
+
